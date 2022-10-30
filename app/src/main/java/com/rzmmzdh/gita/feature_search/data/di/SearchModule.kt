579295c2d327b4fff3b86afdf83072539d96fce0
rzmmzdh <rzmmzdh@gmail.com>
2022-10-30 23:37:14 +0330

    add search module

---
+++ app/src/main/java/com/rzmmzdh/gita/feature_search/data/di/SearchModule.kt
@@ -0,0 +1,60 @@
+package com.rzmmzdh.gita.feature_search.data.di
+
+import com.rzmmzdh.gita.feature_search.data.SearchRepositoryImpl
+import com.rzmmzdh.gita.feature_search.data.datasource.remote.GitHubRemoteService
+import com.rzmmzdh.gita.feature_search.data.datasource.remote.RemoteDataSourceImpl
+import com.rzmmzdh.gita.feature_search.domain.datasource.RemoteDataSource
+import com.rzmmzdh.gita.feature_search.domain.repository.SearchRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class SearchModule {
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        val logger = HttpLoggingInterceptor()
+        logger.setLevel(HttpLoggingInterceptor.Level.BASIC)
+        return OkHttpClient.Builder().addInterceptor(logger)
+            .addInterceptor(Interceptor { chain ->
+                val request = chain.request()
+                request.newBuilder()
+                    .addHeader(
+                        "Authorization",
+                        "Bearer " + "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"
+                    ).build()
+                return@Interceptor chain.proceed(request)
+            }).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemoteClient(httpClient: OkHttpClient): GitHubRemoteService {
+        return Retrofit.Builder().baseUrl("https://api.github.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient)
+            .build()
+            .create(GitHubRemoteService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemoteSearchDataSource(apiService: GitHubRemoteService): RemoteDataSource {
+        return RemoteDataSourceImpl(apiService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSearchRepository(dataSource: RemoteDataSource): SearchRepository {
+        return SearchRepositoryImpl(dataSource)
+    }
+}
