3c1454a9408f4ac7757949158a1c70b45c11be18
rzmmzdh <rzmmzdh@gmail.com>
2022-10-30 21:09:46 +0330

    define jb mono font family

--- app/src/main/java/com/rzmmzdh/gita/theme/Type.kt
+++ app/src/main/java/com/rzmmzdh/gita/theme/Type.kt
@@ -2,11 +2,18 @@ package com.rzmmzdh.gita.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.rzmmzdh.gita.R
 
-// Set of Material typography styles to start with
+val jbMono = FontFamily(
+    Font(R.font.jb_mono_regular, FontWeight.Normal),
+    Font(R.font.jb_mono_light, FontWeight.Light),
+    Font(R.font.jb_mono_bold, FontWeight.Bold),
+    Font(R.font.jb_mono_black, FontWeight.Black)
+)
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
