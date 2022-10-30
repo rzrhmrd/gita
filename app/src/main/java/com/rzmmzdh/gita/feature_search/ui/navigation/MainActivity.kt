190a5421a09cbb1cc7ca8a9283e457bfd6b82bcb
rzmmzdh <rzmmzdh@gmail.com>
2022-10-30 23:47:50 +0330

    hide system ui

--- app/src/main/java/com/rzmmzdh/gita/feature_search/ui/navigation/MainActivity.kt
+++ app/src/main/java/com/rzmmzdh/gita/feature_search/ui/navigation/MainActivity.kt
@@ -3,16 +3,20 @@ package com.rzmmzdh.gita.feature_search.ui.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.rzmmzdh.gita.feature_search.ui.navigation.Destination
-import com.rzmmzdh.gita.feature_search.ui.search_repositories.SearchRepositoriesScreen
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.rzmmzdh.gita.core.theme.GitaTheme
+import com.rzmmzdh.gita.feature_search.ui.search_repositories.SearchRepositoriesScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -34,8 +38,23 @@ class MainActivity : ComponentActivity() {
                             SearchRepositoriesScreen(navController)
                         }
                     }
+                    HideSystemUi()
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun HideSystemUi() {
+        val systemUiController = rememberSystemUiController()
+        val useDarkIcons = !isSystemInDarkTheme()
+
+        DisposableEffect(systemUiController, useDarkIcons) {
+            systemUiController.setSystemBarsColor(
+                color = Color.Transparent,
+                darkIcons = useDarkIcons
+            )
+            onDispose {}
         }
     }
 }
