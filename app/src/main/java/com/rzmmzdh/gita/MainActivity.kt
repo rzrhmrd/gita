d92b39a8c3b4eb04def548379cae3320cf6746ee
rzmmzdh <rzmmzdh@gmail.com>
2022-10-30 21:07:23 +0330

    implement search repositories screen navigation

--- app/src/main/java/com/rzmmzdh/gita/MainActivity.kt
+++ app/src/main/java/com/rzmmzdh/gita/MainActivity.kt
@@ -6,38 +6,36 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.rzmmzdh.gita.feature_search.ui.Destination
+import com.rzmmzdh.gita.feature_search.ui.search_repositories_screen.SearchRepositoriesScreen
 import com.rzmmzdh.gita.theme.GitaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             GitaTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NavHost(
+                        navController = navController,
+                        startDestination = Destination.SearchRepositories.route
+                    ) {
+                        composable(Destination.SearchRepositories.route) {
+                            SearchRepositoriesScreen(navController)
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GitaTheme {
-        Greeting("Android")
     }
 }
