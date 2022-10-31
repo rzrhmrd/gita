381e6aeae3591e8d3da3c16bd0d202b52f3ccc11
rzmmzdh <rzmmzdh@gmail.com>
2022-10-31 08:51:13 +0330

    delegate states

--- app/src/main/java/com/rzmmzdh/gita/feature_search/ui/search_repositories/SearchRepositoriesViewModel.kt
+++ app/src/main/java/com/rzmmzdh/gita/feature_search/ui/search_repositories/SearchRepositoriesViewModel.kt
@@ -1,6 +1,8 @@
 package com.rzmmzdh.gita.feature_search.ui.search_repositories
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.rzmmzdh.gita.feature_search.domain.model.Result
@@ -15,47 +17,42 @@ import javax.inject.Inject
 @HiltViewModel
 class SearchRepositoriesViewModel @Inject constructor(private val searchRepo: SearchRepositories) :
     ViewModel() {
-    var searchPlaceholder = mutableStateOf("search_")
-        private set
-    var searchResult = mutableStateOf(SearchResultUiState())
+    var searchResult by mutableStateOf(SearchResultUiState())
         private set
 
-    var searchQuery = mutableStateOf("")
+    var searchQuery by mutableStateOf("")
         private set
     var searchJob: Job? = null
         private set
-    var errorMessage = MutableStateFlow("")
+    var errorMessage by mutableStateOf("")
         private set
 
-
-    fun onEvent(event: SearchRepositoriesEvent) {
-        when (event) {
-            is SearchRepositoriesEvent.OnSearchQueryChange -> {
-                searchQuery.value = event.value
-                if (event.value.isBlank()) {
-                    searchResult.value = searchResult.value.copy(data = null)
-                }
-                search(event.value)
-            }
-        }
-    }
-
     private fun search(query: String) {
-        searchQuery.value = query
+        searchQuery = query
         searchJob?.cancel()
         searchJob = viewModelScope.launch {
             delay(300L)
             searchRepo(query).collectLatest { result ->
                 when (result) {
-                    is Result.Error -> errorMessage.value =
-                        result.exception?.localizedMessage.toString()
-                    is Result.Loading -> searchResult.value =
-                        searchResult.value.copy(isLoading = true)
-                    is Result.Success -> searchResult.value =
-                        searchResult.value.copy(data = result.data, isLoading = false)
+                    is Result.Error -> {
+                        errorMessage = result.exception?.localizedMessage.toString()
+                        searchResult = searchResult.copy(isLoading = false)
+                    }
+
+                    is Result.Loading -> searchResult =
+                        searchResult.copy(isLoading = true)
+                    is Result.Success -> searchResult =
+                        searchResult.copy(data = result.data, isLoading = false)
                 }
             }
         }
     }
 
+    fun onQueryChange(value: String) {
+        searchQuery = value
+        if (value.isBlank()) {
+            searchResult = searchResult.copy(data = null)
+        }
+        search(value)
+    }
 }
