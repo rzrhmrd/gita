630793cb6c4c333c84257aa2690870a7e796822f
rzmmzdh <rzmmzdh@gmail.com>
2022-10-31 19:41:58 +0330

    rename SearchRepository to GitHubRepository

--- app/src/main/java/com/rzmmzdh/gita/feature_search/domain/usecase/SearchRepositories.kt
+++ app/src/main/java/com/rzmmzdh/gita/feature_search/domain/usecase/SearchRepositories.kt
@@ -2,11 +2,11 @@ package com.rzmmzdh.gita.feature_search.domain.usecase
 
 import com.rzmmzdh.gita.feature_search.domain.model.RepositorySearchResult
 import com.rzmmzdh.gita.feature_search.domain.model.Result
-import com.rzmmzdh.gita.feature_search.domain.repository.SearchRepository
+import com.rzmmzdh.gita.feature_search.domain.repository.GitHubRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class SearchRepositories @Inject constructor(private val repository: SearchRepository) {
+class SearchRepositories @Inject constructor(private val repository: GitHubRepository) {
     suspend operator fun invoke(query: String): Flow<Result<RepositorySearchResult>> {
         return repository.search(query)
     }
