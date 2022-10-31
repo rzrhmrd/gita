492870deb35cd4b4bc1565d3af8694250c824b08
rzmmzdh <rzmmzdh@gmail.com>
2022-10-31 08:53:36 +0330

    remove search repositories event

--- app/src/main/java/com/rzmmzdh/gita/feature_search/ui/search_repositories/SearchRepositoriesEvent.kt
+++
@@ -1,5 +0,0 @@
-package com.rzmmzdh.gita.feature_search.ui.search_repositories
-
-sealed class SearchRepositoriesEvent {
-    data class OnSearchQueryChange(val value: String) : SearchRepositoriesEvent()
-}
