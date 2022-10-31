72d012995d9654f07e49ab5021e773259ce0a271
rzmmzdh <rzmmzdh@gmail.com>
2022-10-31 21:33:49 +0330

    animate screen navigation

--- app/src/main/java/com/rzmmzdh/gita/Entry.kt
+++ app/src/main/java/com/rzmmzdh/gita/Entry.kt
@@ -3,6 +3,9 @@ package com.rzmmzdh.gita
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -12,11 +15,10 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgs
 import androidx.navigation.navArgument
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.rzmmzdh.gita.core.theme.GitaTheme
 import com.rzmmzdh.gita.feature_search.ui.navigation.Destination
@@ -26,20 +28,34 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class Entry : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
+            val navController = rememberAnimatedNavController()
             GitaTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NavHost(
+                    AnimatedNavHost(
                         navController = navController,
                         startDestination = Destination.SearchRepositories.route
                     ) {
-                        composable(Destination.SearchRepositories.route) {
+                        composable(
+                            Destination.SearchRepositories.route,
+                            enterTransition = {
+                                slideIntoContainer(
+                                    AnimatedContentScope.SlideDirection.Down,
+                                    animationSpec = tween(500)
+                                )
+                            },
+                            exitTransition = {
+                                slideOutOfContainer(
+                                    AnimatedContentScope.SlideDirection.Up,
+                                    animationSpec = tween(500)
+                                )
+                            }) {
                             SearchRepositoriesScreen(navController)
                         }
                         composable(
@@ -48,7 +64,19 @@ class Entry : ComponentActivity() {
                                 type = NavType.StringType
                                 defaultValue = "repo"
                             }
-                            )) {
+                            ),
+                            enterTransition = {
+                                slideIntoContainer(
+                                    AnimatedContentScope.SlideDirection.Up,
+                                    animationSpec = tween(500)
+                                )
+                            },
+                            exitTransition = {
+                                slideOutOfContainer(
+                                    AnimatedContentScope.SlideDirection.Down,
+                                    animationSpec = tween(500)
+                                )
+                            }) {
                             RepositoryDetailScreen(navController)
                         }
                     }
